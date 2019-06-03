3c666cb5a5c100499fd87ce0a28d56b94d7fc7e6
Yevgenii Pasko <yevgenii.pasko@mentormate.com>
2019-06-03 11:18:29 +0300

    Initial Commit

---
+++ ProgressStages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgressStages
+//
+//  Created by Yevgenii Pasko on 6/3/19.
+//  Copyright © 2019 Yevgenii Pasko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
